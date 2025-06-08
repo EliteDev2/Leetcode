e09f9673b9d31a9ad45cfe8d5ea43aea2f76c5b3
ELITe <41290701+EliteDev2@users.noreply.github.com>
2025-06-08 18:46:38 +0530

    Time: 11 ms (44.47%) | Memory: 19.4 MB (26.33%) - LeetSync

---
+++ medium/324-wiggle-sort-ii/wiggle-sort-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        sorted_nums = sorted(nums)
+        n = len(nums)
+        mid = (n - 1) // 2
+        last = n - 1
+
+        for i in range(n):
+            if i % 2 == 0:
+                nums[i] = sorted_nums[mid]
+                mid -= 1
+            else:
+                nums[i] = sorted_nums[last]
+                last -= 1
+
+
+        
